43e25f475012491549850bd2e51dfcf8bf6b3487
Dia <Dia@MacBook-Air-de-samuel.local>
2026-06-21 12:48:54 +0200

    vase

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+	private:
+		const std::string	_name;
+		int			_grade;
+
+	public:
+		const std::string	&getName() const;
+		int			getGrade() const;
+		void			incrementGrade();
+		void			decrementGrade();
+
+};
