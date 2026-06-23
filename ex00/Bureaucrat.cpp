3dc0d9663f39c587f5b882a3f566916bd08ea584
Dia <Dia@MacBook-Air-de-samuel.local>
2026-06-23 21:44:48 +0200

    os a moelle

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -23,11 +23,38 @@ Bureaucrat::Bureaucrat(const Bureaucrat& other): _name(other._name), _grade(othe
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
 {
+	std::cout << "Bureaucrat assignment operator called" << std::endl;
 	if (this != &other)
 		_grade = other._grade;
 	return *this;
 }
 
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+const std::string& Bureaucrat::getName() const
+{
+	return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+	return _grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (_grade <= 1)
+		throw GradeTooHighException();
+	_grade--;
+}
+void Bureaucrat::decrementGrade()
+{
+	if (_grade >= 150)
+		throw GradeTooLowException();
+	_grade++;
+}
 const char* Bureaucrat::GradeTooHighException::what() const throw()
 {
 	return "grade too high";
@@ -38,7 +65,8 @@ const char* Bureaucrat::GradeTooLowException::what() const throw()
 	return "grade too low";
 }
 
-Bureaucrat::~Bureaucrat()
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat)
 {
-	std::cout << "Bureaucrat destructor called" << std::endl;
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << "."; 
+	return(out);
 }
