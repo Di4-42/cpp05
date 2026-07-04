2348472b9f3647f26156c33e31cdd9a6683e4f8b
Dia <Dia@MacBook-Air-de-samuel.local>
2026-07-04 19:22:57 +0200

    maitre gims

---
+++ ex01/Bureaucrat.cpp
@@ -0,0 +1,90 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Defaut"), _grade(150) 
+{
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+
+	if (grade > 150)
+		throw GradeTooLowException();
+
+	_grade = grade;
+
+	std::cout << "Bureaucrat parameter constructor called" << std::endl;
+
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other): _name(other._name), _grade(other._grade)
+{
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	std::cout << "Bureaucrat assignment operator called" << std::endl;
+	if (this != &other)
+		_grade = other._grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+const std::string& Bureaucrat::getName() const
+{
+	return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+	return _grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (_grade <= 1)
+		throw GradeTooHighException();
+	_grade--;
+}
+void Bureaucrat::decrementGrade()
+{
+	if (_grade >= 150)
+		throw GradeTooLowException();
+	_grade++;
+}
+void Bureaucrat::signForm(Form& form) const
+{
+	try
+	{
+		form.beSigned(*this);
+		std::cout << _name << " signed " << form.getName() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << _name << " couldn't sign " << form.getName() << " because " << e.what() << std::endl;
+	}
+	
+
+}
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "grade too high";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "grade too low";
+}
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat)
+{
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << "."; 
+	return(out);
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,35 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main()
+{
+	try
+	{
+		Bureaucrat boss("Boss", 0);
+	} 
+	catch (std::exception& e) //hérite de std::exception
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat apayot("Apayot", 151);
+		std::cout << apayot << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	try
+	{
+		Bureaucrat namee("Namee", 47);
+		std::cout << namee << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
